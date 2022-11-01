ad3eb7b3f4b5a8a634b634cd5851fd0a3191a9dd
Jesse Phipps <jessefphipps@gmail.com>
2022-10-31 20:37:51 -0500

    Added favorite movies to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,12 @@ more_movies = [
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
     :release_date => '13-Aug-2021'}
+  {:title => 'Interstellar', :rating => 'PG-13',
+    :release_date => '26-Oct-2014'}
+  {:title => 'The Social Network', :rating => 'PG-13',
+    :release_date => '01-Oct-2010'}
+  {:title => 'Molly\'s Game', :rating => 'R',
+    :release_date => '08-Sep-2017'}
 ]
 
 more_movies.each do |movie|
