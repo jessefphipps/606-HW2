d3923811a96745d309bd4d429f1be2774894f9f2
Jesse Phipps <jessefphipps@gmail.com>
2022-10-31 20:43:50 -0500

    Fixed commas in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
   {:title => 'Interstellar', :rating => 'PG-13',
-    :release_date => '26-Oct-2014'}
+    :release_date => '26-Oct-2014'},
   {:title => 'The Social Network', :rating => 'PG-13',
-    :release_date => '01-Oct-2010'}
+    :release_date => '01-Oct-2010'},
   {:title => 'Molly\'s Game', :rating => 'R',
-    :release_date => '08-Sep-2017'}
+    :release_date => '08-Sep-2017'},
 ]
 
 more_movies.each do |movie|
